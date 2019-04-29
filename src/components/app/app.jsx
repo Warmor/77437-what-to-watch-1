e32709d6e09f68f9c6ab4b85ce5be6c2c22ed458
Grishin Pavel <pg@duotek.ru>
2019-04-30 00:17:15 +0300

    module1-task3

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import WelcomeScreen from "@/components/welcome-screen/welcome-screen";
+
+class App extends React.Component {
+  render() {
+    return <WelcomeScreen/>;
+  }
+}
+
+export default App;

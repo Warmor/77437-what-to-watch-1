85b51dd5483a0180a01aa4c69bc9e4424fe033f0
Grishin Pavel <pg@duotek.ru>
2019-05-05 23:17:09 +0300

    Замечания по предыдущему заданию
    Вынес filmsList в отдельный файл и добавил ещё 1 фильм

---
+++ src/mocks.js
@@ -0,0 +1,17 @@
+export const filmsList = [{
+  id: 1,
+  title: `Fantastic Beasts`,
+  image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+}, {
+  id: 2,
+  title: `Bohemian Rhapsody`,
+  image: `img/bohemian-rhapsody.jpg`
+}, {
+  id: 3,
+  title: `Macbeth`,
+  image: `img/macbeth.jpg`
+}, {
+  id: 4,
+  title: `Aviator`,
+  image: `img/aviator.jpg`
+}];

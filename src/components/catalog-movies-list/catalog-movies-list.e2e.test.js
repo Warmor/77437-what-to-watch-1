ace950eb51e1d54b918ffcf0bace65a113916520
Grishin Pavel <pg@duotek.ru>
2019-05-25 18:04:07 +0300

    refactor e2e test

--- src/components/catalog-movies-list/catalog-movies-list.e2e.test.js
+++ src/components/catalog-movies-list/catalog-movies-list.e2e.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import CatalogMoviesList from "~/components/catalog-movies-list/catalog-movies-list";
+import {CatalogMoviesList} from "~/components/catalog-movies-list/catalog-movies-list";
 
 Enzyme.configure({adapter: new Adapter()});
 
@@ -14,21 +14,21 @@ describe(`<CatalogMoviesList/>`, () => {
 
   it(`CatalogMoviesList correctly click article title`, () => {
     const clickHandler = jest.fn();
-    const app = mount(<CatalogMoviesList filmsList={filmsList} onArticleTitleClick={clickHandler}/>);
+    const app = mount(<CatalogMoviesList films={filmsList} onArticleTitleClick={clickHandler}/>);
     const articleTitle = app.find(`.small-movie-card__link`);
     articleTitle.simulate(`click`, {preventDefault() {}});
     expect(clickHandler).toHaveBeenCalledTimes(1);
   });
 
   it(`CatalogMoviesList correctly set state.activeFilm`, () => {
-    const app = mount(<CatalogMoviesList filmsList={filmsList}/>);
+    const app = mount(<CatalogMoviesList films={filmsList}/>);
     const moviesCard = app.find(`.small-movie-card.catalog__movies-card`);
     moviesCard.simulate(`mouseEnter`);
     expect(app.state(`activeFilm`)).toEqual(filmsList[0]);
   });
 
   it(`CatalogMoviesList correctly remove state.activeFilm`, () => {
-    const app = mount(<CatalogMoviesList filmsList={filmsList}/>);
+    const app = mount(<CatalogMoviesList films={filmsList}/>);
     const moviesCard = app.find(`.small-movie-card.catalog__movies-card`);
     moviesCard.simulate(`mouseLeave`);
     expect(app.state(`activeFilm`)).toEqual(null);
--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Enzyme, {mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "~/reducer";
 import WelcomeScreen from "~/components/welcome-screen/welcome-screen";
 
+
 Enzyme.configure({adapter: new Adapter()});
+const store = createStore(reducer);
 
 describe(`<WelcomeScreen/>`, () => {
-  const filmsList = [{
-    id: 1,
-    title: `Fantastic Beasts`,
-    image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
-  }];
   it(`WelcomeScreen correctly click article title`, () => {
     const clickHandler = jest.fn();
-    const app = mount(<WelcomeScreen filmsList={filmsList} onArticleTitleClick={clickHandler}/>);
-    const articleTitle = app.find(`.small-movie-card__link`);
+    const app = mount(<Provider store={store}>
+      <WelcomeScreen onArticleTitleClick={clickHandler}/>
+    </Provider>);
+    const articleTitle = app.find(`.small-movie-card__link`).at(0);
     articleTitle.simulate(`click`, {preventDefault() {}});
     expect(clickHandler).toHaveBeenCalledTimes(1);
   });

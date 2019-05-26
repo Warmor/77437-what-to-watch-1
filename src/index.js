278f58df6b3c491394605532fc6413c47556b8b1
Grishin Pavel <pg@duotek.ru>
2019-05-26 16:28:03 +0300

    Redux DevTools

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import {Provider} from "react-redux";
 import {reducer} from "~/reducer";
 
 import App from '~/components/app/app.jsx';
-const store = createStore(reducer);
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById(`root`));
